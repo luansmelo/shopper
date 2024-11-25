68fc4e8ea1607c2465f1524ae0b6470f9abdf4d0
luansmelo <luan_decruz19@hotmail.com>
2024-11-25 13:09:33 -0300

    ensure CustomerRouter returns 400 if no name is provided

--- api/src/presentation/routers/customer-router.spec.ts
+++ api/src/presentation/routers/customer-router.spec.ts
@@ -1,6 +1,6 @@
 class CustomerRouter {
     route(httpRequest: any) {
-        if (!httpRequest.body.email) {
+        if (!httpRequest.body.email || !httpRequest.body.name) {
             return {
                 statusCode: 400
             }
@@ -15,6 +15,19 @@ describe('Customer Router', () => {
         const httpRequest = {
             body: {
                 name: 'any_name',
+            }
+        }
+
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse?.statusCode).toBe(400)
+    })
+
+    it('Should return 400 if no name is provided', () => {
+        const sut = new CustomerRouter()
+
+        const httpRequest = {
+            body: {
+                email: 'any_mail@mail.com',
             }
         }
 
