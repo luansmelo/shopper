d9ac2572d43bd44c3793133c6de8034197d91a61
luansmelo <luan_decruz19@hotmail.com>
2024-11-25 14:20:30 -0300

    esure returns 200 if email and name is provided

--- api/src/presentation/routers/customer-router.spec.ts
+++ api/src/presentation/routers/customer-router.spec.ts
@@ -1,9 +1,27 @@
+export interface HttpRequest {
+    query?: unknown;
+    headers?: unknown;
+    body?: unknown;
+    userId?: string;
+    params?: unknown;
+}
+
+export interface HttpResponse {
+    statusCode: number
+}
+
 class CustomerRouter {
-    route(httpRequest: any) {
-        if (!httpRequest.body.email || !httpRequest.body.name) {
+    route(httpRequest: HttpRequest): HttpResponse {
+        const body = httpRequest.body as { email?: string; name?: string };
+
+        if (!body.email || !body.name) {
             return {
                 statusCode: 400
             }
+        }
+
+        return {
+            statusCode: 200
         }
     }
 }
@@ -33,5 +51,19 @@ describe('Customer Router', () => {
 
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse?.statusCode).toBe(400)
+    })
+
+    it('Should return 200 if email and name is provided', () => {
+        const sut = new CustomerRouter()
+
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_mail@mail.com',
+            }
+        }
+
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse?.statusCode).toBe(200)
     })
 })
