8a35131f9016b109c135663d02052743b28f9c17
luansmelo <luan_decruz19@hotmail.com>
2024-11-25 15:56:07 -0300

    ensure returns 500 if no has body

--- api/src/presentation/routers/customer-router.spec.ts
+++ api/src/presentation/routers/customer-router.spec.ts
@@ -14,6 +14,13 @@ export class HttpResponse {
         }
     }
 
+    static serverError() {
+        return {
+            statusCode: 500,
+            body: null,
+        }
+    }
+
     static ok(body: unknown) {
         return {
             statusCode: 200,
@@ -31,6 +38,10 @@ class MissingParamError extends Error {
 
 class CustomerRouter {
     route(httpRequest: HttpRequest) {
+        if (!httpRequest.body) {
+            return HttpResponse.serverError()
+        }
+
         const body = httpRequest.body as { email?: string; name?: string };
 
         if (!body.email) {
@@ -86,5 +97,12 @@ describe('Customer Router', () => {
 
         const httpResponse = sut.route(httpRequest)
         expect(httpResponse?.statusCode).toBe(200)
+    })
+
+    it('Should return 500 if no has body', () => {
+        const sut = new CustomerRouter()
+
+        const httpResponse = sut.route({})
+        expect(httpResponse?.statusCode).toBe(500)
     })
 })
