85004236a3f1cf5c4c4bc0810143e133ae9b4cfa
luansmelo <luan_decruz19@hotmail.com>
2024-11-26 13:55:58 -0300

    add RideModel

---
+++ api/src/infra/db/repositories/models/ride.ts
@@ -0,0 +1,40 @@
+import { Table, Column, Model, DataType, PrimaryKey, ForeignKey, BelongsTo } from "sequelize-typescript";
+import { CustomerModel } from "./customer";
+
+@Table({ tableName: "ride", timestamps: true })
+export class RideModel extends Model {
+    @PrimaryKey
+    @Column({
+        type: DataType.UUID,
+        defaultValue: DataType.UUIDV4,
+    })
+    id!: string;
+
+    @ForeignKey(() => CustomerModel)
+    @Column({ type: DataType.UUID, allowNull: false })
+    customer_id!: string;
+
+    @BelongsTo(() => CustomerModel)
+    customer!: CustomerModel;
+
+    @Column({ type: DataType.DECIMAL, allowNull: false })
+    origin_lat!: number;
+
+    @Column({ type: DataType.DECIMAL, allowNull: false })
+    origin_lon!: number;
+
+    @Column({ type: DataType.DECIMAL, allowNull: false })
+    destination_lat!: number;
+
+    @Column({ type: DataType.DECIMAL, allowNull: false })
+    destination_lon!: number;
+
+    @Column({ type: DataType.DECIMAL, allowNull: false })
+    distance!: number;
+
+    @Column({ type: DataType.STRING, allowNull: false })
+    duration!: string;
+
+    @Column({ type: DataType.JSON, allowNull: true })
+    route_response!: object;
+}
