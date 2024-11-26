70fae7ee271b3c7530df17a4b2a16bb5c41af6a5
luansmelo <luan_decruz19@hotmail.com>
2024-11-25 21:31:41 -0300

    add email validation CustomerRouter

--- api/src/presentation/routers/customer-router.ts
+++ api/src/presentation/routers/customer-router.ts
@@ -1,10 +1,15 @@
+import { EmailValidator } from "@/validators/protocols/email-validator";
 import { MissingParamError } from "../errors/missing-param-error";
 import { badRequest, ok, serverError } from "../helpers/http-helper";
 import { HttpRequest } from "../protocols/http-request.protocol";
 import { HttpResponse } from "../protocols/http-response.protocol";
+import { InvalidParamError } from "../errors/invalid-param-error";
 
 export class CustomerRouter {
-    constructor(private readonly customerUseCase: any) { }
+    constructor(
+        private readonly customerUseCase: any,
+        private readonly validation: EmailValidator
+    ) { }
 
     route(httpRequest: HttpRequest): HttpResponse {
         try {
@@ -12,6 +17,10 @@ export class CustomerRouter {
 
             if (!body.email) {
                 return badRequest(new MissingParamError('email'))
+            }
+
+            if (!this.validation.isValid(body.email)) {
+                return badRequest(new InvalidParamError('email'))
             }
 
             if (!body.name) {
