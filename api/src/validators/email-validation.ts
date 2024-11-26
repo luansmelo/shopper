635efa10eb52867974fbb3a681bb7fa89885d134
luansmelo <luan_decruz19@hotmail.com>
2024-11-25 21:31:20 -0300

    add validator

---
+++ api/src/validators/email-validation.ts
@@ -0,0 +1,17 @@
+import { Validation } from "@/presentation/protocols/validation.protocol"
+import { EmailValidator } from "./protocols/email-validator"
+import { InvalidParamError } from "@/presentation/errors/invalid-param-error"
+
+export class EmailValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly emailValidator: EmailValidator
+  ) {}
+
+  validate (input: any): Error | void {
+    const isValid = this.emailValidator.isValid(input[this.fieldName])
+    if (!isValid) {
+      return new InvalidParamError(this.fieldName)
+    }
+  }
+}
