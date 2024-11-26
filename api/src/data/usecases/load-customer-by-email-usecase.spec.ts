8749e7515272d2d95212cadd0898f5846ef1cae2
luansmelo <luan_decruz19@hotmail.com>
2024-11-26 10:05:20 -0300

    refactor logic

---
+++ api/src/data/usecases/load-customer-by-email-usecase.spec.ts
@@ -0,0 +1,60 @@
+import { LoadCustomerByEmailRepositoryProtocol } from "../protocols/db/load-customer-by-email.repository"
+import { LoadCustomerByIdUseCase } from "./load-customer-by-email-usecase"
+
+class LoadCustomerByEmailRepositorySpy implements LoadCustomerByEmailRepositoryProtocol {
+    async load(email: string): Promise<LoadCustomerByEmailRepositoryProtocol.Result | null> {
+        return new Promise(resolve => resolve({
+            id: 'any_id',
+            name: 'any_name',
+            email: 'any_email'
+        }))
+    }
+}
+
+const makeSut = () => {
+    const loadCustomerByEmailRepository = new LoadCustomerByEmailRepositorySpy()
+    const sut = new LoadCustomerByIdUseCase(loadCustomerByEmailRepository)
+
+    return {
+        sut,
+        loadCustomerByEmailRepository
+    }
+}
+
+describe('LoadCustomerByEmail UseCase', () => {
+    it('Should throw if no email is provided', async () => {
+        const { sut } = makeSut()
+        const promise = sut.load('')
+        expect(promise).rejects.toThrow()
+    })
+
+    it('Should throw if no customer no found', async () => {
+        const { sut, loadCustomerByEmailRepository } = makeSut()
+        jest.spyOn(loadCustomerByEmailRepository, 'load').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        const promise = sut.load('email_fake@mail.com')
+        expect(promise).rejects.toThrow()
+    })
+
+    it('Should call LoadCustomerByEmailRepository with correct email', async () => {
+        const { sut } = makeSut()
+
+        const load = await sut.load('any_email')
+
+        expect(load?.email).toBe('any_email')
+    })
+
+    it('Should return customer data when email is provided', async () => {
+        const { sut } = makeSut()
+
+        const customer = await sut.load('any_email')
+
+        expect(customer).toEqual({
+            id: 'any_id',
+            name: 'any_name',
+            email: 'any_email'
+        })
+    })
+})
