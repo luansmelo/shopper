48e1164f1dd5c28bfea8ab1ab47fdd92ad0e3914
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 10:38:48 -0300

    sequelize initialize

--- api/src/main/server.ts
+++ api/src/main/server.ts
@@ -7,11 +7,6 @@ import { sequelize } from '@/config/database.config'
 const port = Number(process.env.PORT) || 3000
 
 sequelize.sync().then(() => {
-    console.log("Banco de dados conectado e sincronizado!");
-}).catch((error) => {
-    console.error("Erro ao conectar no banco de dados:", error);
-});
+    app.listen(port, () => console.log(`Server running at ${port} port`))
+}).catch(console.error)
 
-app.listen(port, () => {
-    console.log(`Server running at ${port} port`)
-})
