0b58216b15ef13f1de36d74140569764b7ada995
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 01:34:05 -0300

    add sequelize and sequelize-ts and sequelize cli

---
+++ api/src/config/sequelize.config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME
+}
