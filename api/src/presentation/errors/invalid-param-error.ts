e286f92083e24097ba70a5bd1fd9e42a6bbc3f7e
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 15:18:14 -0300

    change name

--- api/src/presentation/errors/invalid-param-error.ts
+++ api/src/presentation/errors/invalid-param-error.ts
@@ -1,6 +1,6 @@
 import { CustomError } from "./custom-error";
 export class InvalidParamError extends CustomError {
     constructor(param: string) {
-        super('INVALID_PARAM', `Invalid parameter: ${param}`);
+        super('INVALID_DATA', `Invalid parameter: ${param}`);
     }
 }
