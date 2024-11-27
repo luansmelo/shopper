b0d49b9d19b01bc17d131b25d6e65fbb65b2387f
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 01:56:42 -0300

    add sequelize sycronize in server

--- api/src/main/server.ts
+++ api/src/main/server.ts
@@ -2,8 +2,16 @@ import 'module-alias/register'
 import dotenv from 'dotenv'
 dotenv.config()
 import app from './config/app'
+import { sequelize } from '@/config/database.config'
 
 const port = Number(process.env.PORT) || 3000
+
+sequelize.sync().then(() => {
+    console.log("Banco de dados conectado e sincronizado!");
+}).catch((error) => {
+    console.error("Erro ao conectar no banco de dados:", error);
+});
+
 app.listen(port, () => {
     console.log(`Server running at ${port} port`)
 })
