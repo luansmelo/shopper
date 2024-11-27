eb805ab02649ae4233d7dfa1e7263099fbd515f6
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 00:25:47 -0300

    remove listen to resolve conflitct port

--- api/src/main/config/app.ts
+++ api/src/main/config/app.ts
@@ -2,7 +2,5 @@ import express from 'express'
 import setupMiddlewares from './middlewares';
 
 const app = express()
-const PORT = 3000
 setupMiddlewares(app)
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
 export default app
--- api/src/main/config/middlewares.ts
+++ api/src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
-import { Express } from 'express';
-import { corsMiddleware } from '../middlewares/cors';
+import { Express } from 'express'
+import { corsMiddleware } from '../middlewares/cors'
+import { bodyParser } from '../middlewares/json-parser'
 
 export default (app: Express): void => {
-	app.use(corsMiddleware);
-};
+	app.use(bodyParser)
+	app.use(corsMiddleware)
+}
