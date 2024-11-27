c4b2c3290f59e78e283a6e4c79d055c936bd2b6f
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 10:46:22 -0300

    add LoadCustomerByEmail on route

--- api/src/main/routes/customer/customer.routes.ts
+++ api/src/main/routes/customer/customer.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { adapt } from "../../adapters/express.adapter";
 import { makeAddCustomerController } from "../../factories/add-customer.factory";
+import { makeLoadCustomerByEmailController } from "@/main/factories/load-customer.factory";
 
 export default (route: Router): void => {
     route.post('/customers', adapt(makeAddCustomerController()))
+    route.get('/customers/:email', adapt(makeLoadCustomerByEmailController()))
 }
