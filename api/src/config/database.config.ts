07d2a43ca2620b3f16a5a2259d0444c7d5586e0d
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 10:38:18 -0300

    add envarioment to test

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -1,12 +1,40 @@
-import { CustomerModel } from "@/infra/db/models/customer";
-import { Sequelize } from "sequelize-typescript";
+import { CustomerModel } from "@/infra/db/models/customer"
+import { Sequelize } from "sequelize-typescript"
 
-export const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    models: [CustomerModel],
-})
+const env = process.env.NODE_ENV || 'development'
+
+const getDatabaseConfig = () => {
+    const commonConfig = {
+        models: [CustomerModel],
+    }
+
+    const config = {
+        development: {
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: Number(process.env.DB_PORT),
+            database: process.env.DB_NAME,
+        },
+        test: {
+            dialect: 'sqlite',
+            storage: ':memory:'
+        },
+        production: {
+            dialect: 'postgres',
+            host: process.env.DB_HOST,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            port: Number(process.env.DB_PORT),
+            database: process.env.DB_NAME
+        },
+    }
+
+    return {
+        ...commonConfig,
+        ...config[env]
+    }
+}
+
+export const sequelize = new Sequelize(getDatabaseConfig())
