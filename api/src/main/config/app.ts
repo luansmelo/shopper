87327723d11a28be8eb24c4af852e448e08a9698
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 00:16:23 -0300

    add app config

--- api/src/main/config/app.ts
+++ api/src/main/config/app.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import setupMiddlewares from './middlewares';
 
 const app = express()
-
+const PORT = 3000
+setupMiddlewares(app)
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
 export default app
