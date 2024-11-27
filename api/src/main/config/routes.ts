95f989584aa8bb6327839a94c8b15a87f06d3217
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 01:25:29 -0300

    remove sourceMap

--- api/src/main/config/routes.ts
+++ api/src/main/config/routes.ts
@@ -3,12 +3,12 @@ import { readdirSync } from 'fs'
 import { join } from 'path'
 
 export default (app: Express): void => {
-    const router = Router()
-    app.use('/api', router)
-    const routesDir = join(__dirname, '../routes')
-    readdirSync(routesDir, { recursive: true }).map(async file => {
-        if (file.includes('.routes.')) {
-            (await import(`${routesDir}/${file}`)).default(router)
-        }
-    })
-}   
+	const router = Router()
+	app.use('/api', router)
+	const routesDir = join(__dirname, '../routes')
+	readdirSync(routesDir, { recursive: true }).map(async file => {
+		if (file.includes('.routes.')) {
+			(await import(`${routesDir}/${file}`)).default(router)
+		}
+	})
+}
