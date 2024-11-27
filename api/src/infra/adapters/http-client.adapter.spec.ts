aa5f74d8008eebd88189a65bbd7c946ecf8f3343
luansmelo <luan_decruz19@hotmail.com>
2024-11-26 21:50:24 -0300

    ensure httpClienteAdapter tests

---
+++ api/src/infra/adapters/http-client.adapter.spec.ts
@@ -0,0 +1,59 @@
+import { HttpClientProtocol } from '../protocols/http-client-protocol'
+import { HttpClientAdapter } from './http-client.adpter'
+import axios from 'axios'
+
+const makeSut = () => {
+    const sut = new HttpClientAdapter()
+    return {
+        sut
+    }
+}
+
+describe('HttpClientAdapter', () => {
+    it('Should call axios with correct parameters', async () => {
+        const { sut } = makeSut()
+
+        const mocked = jest.spyOn(axios, 'request').mockImplementationOnce(() =>
+            Promise.resolve({
+                data: {
+                    name: 'fake_name'
+                }
+            })
+        )
+
+        const params: HttpClientProtocol.Request = {
+            method: 'get',
+            url: 'http://localhost/api',
+            headers: { Authorization: 'Bearer token' },
+            body: {
+                name: 'fake_name'
+            },
+        }
+
+        const response = await sut.request(params)
+
+        expect(mocked).toHaveBeenCalledWith({
+            url: params.url,
+            method: params.method,
+            headers: params.headers,
+            data: params.body,
+        })
+
+        expect(response.data).toEqual({ name: 'fake_name' })
+    })
+
+    it('Should throw an error if axios throws', async () => {
+        const { sut } = makeSut()
+
+        jest.spyOn(axios, 'request').mockRejectedValueOnce(new Error(axios.AxiosError.ERR_NETWORK))
+
+        const params: HttpClientProtocol.Request = {
+            url: 'https://example.com/api',
+            method: 'get',
+            headers: { Authorization: 'Bearer token' },
+            body: null,
+        }
+
+        await expect(sut.request(params)).rejects.toThrow(axios.AxiosError.ERR_NETWORK)
+    })
+})
