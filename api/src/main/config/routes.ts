13e6c9c54cf8a6129f872596c57d74cc9113c50b
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 00:55:36 -0300

    add routes config

---
+++ api/src/main/config/routes.ts
@@ -0,0 +1,14 @@
+import { Express, Router } from 'express'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+export default (app: Express): void => {
+    const router = Router()
+    app.use('/api', router)
+    const routesDir = join(__dirname, '../routes')
+    readdirSync(routesDir, { recursive: true }).map(async file => {
+        if (file.includes('.routes.')) {
+            (await import(`${routesDir}/${file}`)).default(router)
+        }
+    })
+}   
