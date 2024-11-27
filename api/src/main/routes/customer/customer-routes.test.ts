58218e41aebc9ec938e4128190763223b2b0cc49
luansmelo <luan_decruz19@hotmail.com>
2024-11-27 10:39:39 -0300

    ensure CustomerRouter  returns 200 on success create a customer

---
+++ api/src/main/routes/customer/customer-routes.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import app from '../../config/app'
+import { sequelize } from '@/config/database.config'
+import { CustomerModel } from '@/infra/db/models/customer'
+
+describe('Customer Routes', () => {
+    beforeAll(async () => {
+        await sequelize.sync({ force: true })
+    })
+
+    afterAll(async () => {
+        await sequelize.close()
+    })
+
+    beforeEach(async () => {
+        await CustomerModel.truncate()
+    })
+
+    describe('POST /api/customers', () => {
+        it('Should return 200 on success create a customer', async () => {
+            await request(app)
+                .post('/api/customers')
+                .send({
+                    name: 'any_name',
+                    email: 'any_email@hotmail.com'
+                })
+                .expect(200)
+        })
+    })
+})
