b34954b7fead0e2265aee4b9c22c7613a2380c1c
luansmelo <luan_decruz19@hotmail.com>
2024-11-28 01:15:55 -0300

    copy GOOGLE_KEY_API

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, path.resolve(__dirname, '..'), '') 
+  const env = loadEnv(mode, process.cwd(), '') 
 
   return defineConfig({
     server: {
@@ -15,7 +14,7 @@ export default ({ mode }: { mode: string }) => {
     css: {
       postcss: './postcss.config.ts',
     },
-    envDir: path.resolve(__dirname, '..'), 
+    envDir: process.cwd(), 
     define: {
       'process.env': env,
     },
